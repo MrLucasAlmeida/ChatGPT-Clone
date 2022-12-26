984ad50a40dfd4c29d3490a151b063838cef154a
MrLucasAlmeida <lucasoa2701@live.com>
2022-12-26 03:24:03 -0800

    added correct server side url

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetches AI response from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-clone-13gm.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
